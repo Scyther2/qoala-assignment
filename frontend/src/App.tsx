dbc191122ae9d0ab00d6d96859ed63d66b0712b1
Nihar Thummar <Nihar1392002@gmail.com>
2023-12-25 19:46:30 +0530

    added ui and backend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,56 @@
+import {useState} from 'react';
+
+export default function App() {
+
+  const [file, setFile] = useState<File | null>(null);
+  const [data, setData] = useState<string | null>(null);
+
+
+  const handleFileChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0];
+    if(selectedFile instanceof File) setFile(selectedFile);
+  }
+
+  const handleSubmit = async (e : React.FormEvent) => {
+    e.preventDefault();
+    if(!file) return;
+    setUploading(true);
+    const formData = new FormData();
+    formData.append("image", file);
+
+    try {
+      const response = await fetch("http://localhost:3000/detectText", {
+        method: "POST",
+        body: formData
+      });
+
+      const responseData = await response.json();
+      setData(responseData);
+
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setUploading(false);
+    }
+    
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-around p-24">
+
+      <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 pb-6 pt-8">
+        Qoala OCR Assignment
+      </p>
+
+      <form className='flex items-center justify-center space-x-4' onSubmit={handleSubmit}>
+        <input className='border-2 border-white p-2 rounded-lg' name='image' onChange={handleFileChange} type="file" accept="image/*" />
+        <button className='border-2 border-white p-2 rounded-lg' type="submit">
+          Upload
+        </button>
+      </form>
+    
+      {data && <pre className='border-2 border-white py-4 px-6 rounded-lg w-full max-w-5xl'>JSON Data: {JSON.stringify(data, null, 4)}</pre>}
+
+    </main>
+  )
+}
---
+++ index.js
@@ -0,0 +1,136 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import multer from 'multer';
+import detectText from './utils/vision.js';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import dotenv from 'dotenv';
+import Card from'./models/ocr-data.js';
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+// strict mode of mongodb is off
+mongoose.set("strictQuery", true);
+
+// Set up multer storage to save the file to disk
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const uploadPath = path.join(__dirname, 'uploads');
+        console.log("uploadPath:", uploadPath);
+        fs.mkdirSync(uploadPath, { recursive: true });
+        cb(null, uploadPath);
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+const upload = multer({ storage: storage });
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+async function handlePostRequest(req, res) {
+    try {
+        const imageFile = req.file;
+
+        if (!imageFile) {
+            return res.status(400).json({ error: 'No image uploaded. Retry with uploading an image.' });
+        }
+
+        // detect text from uploaded image
+        const receivedText = await detectText(imageFile.path);
+        // upload result to db
+
+        const response = await fetch('http://localhost:3000/createRecord', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ data: receivedText }),
+        });
+        console.log(response.status, response.statusText);
+
+        return res.status(200).send({ message: 'File uploaded successfully!', text: receivedText });
+        
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({ error: error });
+    }
+}
+
+app.post('/detectText', upload.single('image'), handlePostRequest);
+//   (req,res) => {
+//     res.json({
+//       success: 1,
+//       profile_url: `http://localhost:3000/uploads/${req.file.filename}`
+//     });
+//   }
+//  );
+
+// Create a record
+app.post('/createRecord', async (req, res) => {
+    try {
+      const { data } = req.body;
+      const newRecord = new Card(
+        { 
+            name: data.name, 
+            lastName: data.lastName, 
+            dateOfBirth: data.dateOfBirth, 
+            identificationNumber: data.identificationNumber, 
+            dateOfIssue: data.dateOfIssue, 
+            dateOfExpiry: data.dateOfExpiry 
+        });
+      const savedRecord = await newRecord.save();
+      res.status(201).json(savedRecord);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+// Get all records
+app.get('/getAllRecords', async (req, res) => {
+    try {
+        const allRecords = await Card.find();
+        
+      if (!allRecords) {
+        return res.status(404).json({ message: 'Records not found' });
+      }
+      res.json({ records: allRecords });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+app.get('*', (req, res) => {
+    res.send('Hello, OCR Server is running! Please make a POST request to /detectText to detect text in an image.');
+});
+
+// connect to database
+async function connectDB() {
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URI);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (err) {
+      console.log(err);
+      process.exit(1);
+    }
+  }
+  
+  function startListening() {
+    app.listen(PORT, () => {
+      console.log(`server is running on port ${PORT}`);
+    });
+  }
+  
+  connectDB().then(startListening());
+  
+  console.log("hi!");
+  
